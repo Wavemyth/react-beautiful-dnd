2f7e161c3696d77fcffcbd97727ce1bfec9b5eb9
unknown <ionut@wavemyth.com>
2024-05-22 11:27:24 +0300

    avoid peerdependency issue

--- src/view/drag-drop-context/use-startup-validation.js
+++ src/view/drag-drop-context/use-startup-validation.js
@@ -1,13 +1,14 @@
 // @flow
 import React from 'react';
-import { peerDependencies } from '../../../package.json';
 import checkReactVersion from './check-react-version';
 import checkDoctype from './check-doctype';
 import useDevSetupWarning from '../use-dev-setup-warning';
 
+const requiredReactVersion = '^16.8.5 || ^17.0.0 || ^18.0.0'; // Replace this with the actual React version you are using
+
 export default function useStartupValidation() {
   useDevSetupWarning(() => {
-    checkReactVersion(peerDependencies.react, React.version);
+    checkReactVersion(requiredReactVersion, React.version);
     checkDoctype(document);
   }, []);
 }
